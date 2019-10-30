f7ed2bbe2afda50f36c588c7f938eec2f040b44c
Reddy57 <abhilash.gaddam@antra.com>
2019-10-30 10:19:02 -0400

    Context changes

--- src/MovieShop.Data/MovieShopDbContext.cs
+++ src/MovieShop.Data/MovieShopDbContext.cs
@@ -24,7 +24,7 @@ namespace MovieShop.Data
             // ToTable(table) method is used to define the Table name for Entity Class, in this case we are creating Genre table. Equivalent to the Table attribute
             builder.ToTable("Genre");
 
-            // HasKey(selector) method takes lambda expression that selects the primary key for our Table, in our case we want Id as primary Key. It is similar to [Key] attribute in dataannotations.
+            // HasKey(selector) method takes lambda expression that selects the primary key for our Table, in our case we want Id as primary Key. It is similar to [Key] attribute in data annotations.
             builder.HasKey(g => g.Id);
 
             // Property(selector) is used to describe more details about property or column in our table, like making it not null or restricting the maximum length etc and many more.
